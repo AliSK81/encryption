b5c337d66af3ccf2b913b0f58ef8496daa44b6df
Mohammad_Es <mohammad.es0281@gmail.com>
2023-02-20 22:22:35 +0330

    add number of columns to rail fence cipher

--- algorithms/1-symmetric/1-traditional/2-transposition/1-keyless/1-rail-fence-cipher.py
+++ algorithms/1-symmetric/1-traditional/2-transposition/1-keyless/1-rail-fence-cipher.py
@@ -1,30 +1,26 @@
-def encrypt(txt: str) -> str:
+import math
+
+
+def encrypt(txt: str, num_of_columns: int) -> str:
     cipher = ''
 
-    for i in range(0, len(txt), 2):
-        cipher += txt[i]
-
-    for i in range(1, len(txt), 2):
-        cipher += txt[i]
+    for i in range(0, num_of_columns):
+        for j in range(i, len(txt), num_of_columns):
+            cipher += txt[j]
 
     return cipher
 
 
-def decrypt(cipher: str) -> str:
+def decrypt(cipher: str, num_of_columns: int) -> str:
     txt = ''
 
-    mid = (len(cipher) + 1) // 2
-
-    for i in range(mid):
-        txt += cipher[i]
-
-        j = i + mid
-
-        if j < len(cipher):
+    num_of_row = math.ceil(len(cipher) / num_of_columns)
+    for i in range(0, num_of_row):
+        for j in range(i, len(cipher), num_of_row):
             txt += cipher[j]
 
     return txt
 
 
 if __name__ == '__main__':
-    print(decrypt(encrypt(txt='plaintext')))
+    print(decrypt(encrypt(txt='plaintext', num_of_columns=3), num_of_columns=3))
