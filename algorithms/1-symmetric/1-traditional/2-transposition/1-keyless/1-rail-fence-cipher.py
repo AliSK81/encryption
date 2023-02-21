024f2082ed8e79872b9d35c1c8798ccbfd6db65c
Mohammad_Es <mohammad.es0281@gmail.com>
2023-02-21 09:34:20 +0330

    refactor rail fence cipher

--- algorithms/1-symmetric/1-traditional/2-transposition/1-keyless/1-rail-fence-cipher.py
+++ algorithms/1-symmetric/1-traditional/2-transposition/1-keyless/1-rail-fence-cipher.py
@@ -1,26 +1,26 @@
 import math
 
 
-def encrypt(txt: str, num_of_columns: int) -> str:
+def encrypt(txt: str, key: int) -> str:
     cipher = ''
 
-    for i in range(0, num_of_columns):
-        for j in range(i, len(txt), num_of_columns):
+    for i in range(0, key):
+        for j in range(i, len(txt), key):
             cipher += txt[j]
 
     return cipher
 
 
-def decrypt(cipher: str, num_of_columns: int) -> str:
+def decrypt(cipher: str, key: int) -> str:
     txt = ''
 
-    num_of_row = math.ceil(len(cipher) / num_of_columns)
-    for i in range(0, num_of_row):
-        for j in range(i, len(cipher), num_of_row):
+    rows = math.ceil(len(cipher) / key)
+    for i in range(0, rows):
+        for j in range(i, len(cipher), rows):
             txt += cipher[j]
 
     return txt
 
 
 if __name__ == '__main__':
-    print(decrypt(encrypt(txt='plaintext', num_of_columns=3), num_of_columns=3))
+    print(decrypt(encrypt(txt='plaintext', key=3), key=3))
